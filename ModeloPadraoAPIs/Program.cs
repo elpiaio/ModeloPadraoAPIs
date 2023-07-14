0b2938115c9710e34a42630f87a86a395c497b56
WillScapin <Willian.scapin@skysoftware.com.br>
2023-07-14 14:32:54 -0300

    Corrigindo Api

--- ModeloPadraoAPIs/Program.cs
+++ ModeloPadraoAPIs/Program.cs
@@ -13,37 +13,45 @@ using ModeloPadraoAPIs.Interfaces;
 using ModeloPadraoAPIs.Services;
 using ModeloPadraoAPIs.REST;
 using ModeloPadraoAPIs.Mappings;
+using Microsoft.Graph.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-
-var builder = Microsoft.Graph.Models.WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<IntEnderecoServices, EnderecoService>();
-builder.Services.AddSingleton<IntBancoServices, BancoService>();
-builder.Services.AddSingleton<IntBrasilApi, BrasilApiRest>();
-
-builder.Services.AddAutoMapper(typeof(EnderecoMapping));
-builder.Services.AddAutoMapper(typeof(BancoMapping));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddSingleton<IntEnderecoServices, EnderecoService>();
+        builder.Services.AddSingleton(new BancoService());
+        builder.Services.AddSingleton<IntBrasilApi, BrasilApiRest>();
+
+        builder.Services.AddAutoMapper(typeof(EnderecoMapping));
+        builder.Services.AddAutoMapper(typeof(BancoMapping));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
